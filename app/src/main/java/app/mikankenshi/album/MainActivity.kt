84e0b411b989ee8a7ce54c874c61a497be63d4e9
redsharei <syarei@live.jp>
2019-11-08 12:45:36 +0900

    windows shortcuts registered

--- app/src/main/java/app/mikankenshi/album/MainActivity.kt
+++ app/src/main/java/app/mikankenshi/album/MainActivity.kt
@@ -9,6 +9,7 @@ import kotlinx.android.synthetic.main.activity_main.*
 import app.mikankenshi.album.R
 
 class MainActivity : AppCompatActivity() {
+    //var num = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,48 +17,103 @@ class MainActivity : AppCompatActivity() {
 
         var intent: Intent = Intent(this, Preview::class.java)
 
-        val names = arrayOf("A","B","C")
-        val items = Array(names.size, {i -> names[i]})
+        val names = arrayOf(
+            "スタートメニューを表示",
+            "デスクトップを表示",
+            "Cortanaを起動",
+            "設定画面を表示",
+            "アクションセンターを表示",
+            "Windows Ink Workspaceを起動",
+            "タスクバーからアプリを起動",
+            "アプリやウィンドウを切り替え",
+            "タスクビューを表示",
+            "仮装デスクトップを追加",
+            "仮想デスクトップを切り替え",
+            "仮想デスクトップを閉じる",
+            "操作を元に戻す",
+            "元に戻した操作をやり直す",
+            "すべての項目を選択",
+            "複数の項目を選択",
+            "切る取り",
+            "コピー",
+            "貼り付け",
+            "クリップボードの履歴を表示",
+            "新規ウィンドウを開く",
+            "ファイルを保存",
+            "ファイルを開く",
+            "ファイルを印刷",
+            "ショートカットメニューを表示",
+            "エクスプローラーを起動",
+            "アイコンの表示形式を変更",
+            "前のフォルダに戻る",
+            "戻る前のフォルダに進む",
+            "親フォルダに移動",
+            "フォルダ名を変更",
+            "新しいフォルダを作成",
+            "フォルダを検索",
+            "リボンを表紙",
+            "複数項目を選択",
+            "フォルダを完全削除",
+            "ウィンドウを閉じる",
+            "プロパティを表示",
+            "プレビューパネルを表示",
+            "アドレスバーに履歴を表示",
+            "ウィンドウのメニューを表示",
+            "ウィンドウを最大化・最小化",
+            "ウィンドウを左半分・右半分に合わせる",
+            "全てのウィンドウを最小化",
+            "ウィンドウを閉じる",
+            "画面表示のモードを選択",
+            "スクリーンショットを撮影",
+            "スクリーンショットを撮影して保存",
+            "指定した範囲のスクリーンショットを撮影",
+            "ファイル名を指定して実行",
+            "タスクマネージャー",
+            "パソコンをロック",
+            "クイックリンクメニューを表示"
+        )
+        val items = Array(names.size, { i -> names[i] })
 
         val adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items)
         listView.adapter = adapter
 
 
-        listView.setOnItemClickListener { _, view, _, _ ->
-            val textView = view.findViewById<TextView>(android.R.id.text1)
-            intent.putExtra("image",R.drawable.keybord)
+        listView.setOnItemClickListener { _, _, position, _ ->
+            //  val textView = view.findViewById<TextView>(android.R.id.text1)
+            //num = position
+            intent.putExtra("pos", position)
             startActivity(intent)
 
         }
-       /* imageView2.setOnClickListener {
-            intent.putExtra("image",R.drawable.cute)
-            startActivity(intent)
-        }
-        imageView3.setOnClickListener {
-            intent.putExtra("image",R.drawable.cute)
-            startActivity(intent)
-        }
+        /* imageView2.setOnClickListener {
+             intent.putExtra("image",R.drawable.cute)
+             startActivity(intent)
+         }
+         imageView3.setOnClickListener {
+             intent.putExtra("image",R.drawable.cute)
+             startActivity(intent)
+         }
 
-        imageView4.setOnClickListener {
-            intent.putExtra("image",R.drawable.cute)
-            startActivity(intent)
-        }
+         imageView4.setOnClickListener {
+             intent.putExtra("image",R.drawable.cute)
+             startActivity(intent)
+         }
 
-        imageView5.setOnClickListener {
-            intent.putExtra("image",R.drawable.cute)
-            startActivity(intent)
-        }
+         imageView5.setOnClickListener {
+             intent.putExtra("image",R.drawable.cute)
+             startActivity(intent)
+         }
 
-        imageView6.setOnClickListener {
-            intent.putExtra("image",R.drawable.cute)
-            startActivity(intent)
-        }
+         imageView6.setOnClickListener {
+             intent.putExtra("image",R.drawable.cute)
+             startActivity(intent)
+         }
 
-        imageView.setOnClickListener {
-            intent.putExtra("image",R.drawable.cute)
-            startActivity(intent)
-        }
-*/
+         imageView.setOnClickListener {
+             intent.putExtra("image",R.drawable.cute)
+             startActivity(intent)
+         }
+ */
 
     }
 }
--- app/src/main/java/app/mikankenshi/album/Preview.kt
+++ app/src/main/java/app/mikankenshi/album/Preview.kt
@@ -10,8 +10,64 @@ class Preview : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_preview)
 
-        val imageId: Int = intent.getIntExtra("image", 0)
-        preview.setImageResource(imageId)
+        val posId: Int = intent.getIntExtra("pos", 0)
+        val keys = arrayOf("Windows",
+            "Windows + D",
+            "Windows + S",
+            "Windows + I",
+            "Windows + A",
+            "Windows + W",
+            "Windows + 1～0",
+            "Alt + Tab",
+            "Windows + Tab",
+            "Windows + Ctrl + D",
+            "Windows + Ctrl + ←,→",
+            "Windows + Ctrl + F4",
+            "Ctrl + Z",
+            "Ctrl + Y",
+            "Ctrl + A",
+            "Ctrl + 矢印",
+            "Ctrl + X",
+            "Ctrl + C",
+            "Ctrl + V",
+            "Windows + V",
+            "Ctrl + N",
+            "Ctrl + S",
+            "Ctrl + O",
+            "Ctrl + P",
+            "Shift + F10",
+            "Windows + E",
+            "Ctrl +Shift + 1～8",
+            "Alt + ←",
+            "Alt + →",
+            "Alt + ↑",
+            "F2",
+            "Ctrl + Shift + N",
+            "Ctrl + F",
+            "Ctrl + F1",
+            "Ctrl + 矢印 からの Space",
+            "Shift + Delete ",
+            "Alt + F4",
+            "Alt + Enter",
+            "Alt + P",
+            "F4",
+            "Alt + Space",
+            "Windows + ↑,↓",
+            "Windows + ←,→",
+            "Windows + M",
+            "Ctrl + W",
+            "Windows + P",
+            "PrintScreen",
+            "Windows + PrintScreen",
+            "Windows + Shift + S",
+            "Windows + R",
+            "Ctrl + Shift + Eac",
+            "Windows + L",
+            "Windows + X"
+        )
+
+        textView.text = keys[posId]
+        //preview.setImageResource(posId)
 
 
         back.setOnClickListener{
